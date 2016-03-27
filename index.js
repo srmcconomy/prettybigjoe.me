4e3f030ebfbf6593a5cf328f1f1d8dfead3b2d3a
Steven McConomy <srmcconomy@gmail.com>
2016-03-27 17:37:40 -0400

    fixes

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
 app.use(express.static('public'));
 
 app.listen(8999, function() {})
